4ed967ec446bdef4ffccfdb50c61e9797ca84d78
Lars Strojny <lars@strojny.net>
2013-09-23 00:54:42 +0200

    Cleanup

--- src/hmmmath/Percentile/Percentile.php
+++ src/hmmmath/Percentile/Percentile.php
@@ -9,6 +9,15 @@ final class Percentile
 
     const LINEAR_INTERPOLATION = 'linearInterpolation';
 
+    /**
+     * Calculate nth percentile of a list of numbers
+     *
+     * @param number[] $numbers
+     * @param double $percentile
+     * @param string $mode
+     * @param boolean $sort
+     * @return double
+     */
     public static function percentile(array $numbers, $percentile, $mode = self::NEAREST_RANK, $sort = true)
     {
         InvalidArgumentException::assertParameterType('2', 'double', $percentile, '0..1');
@@ -26,11 +35,11 @@ final class Percentile
         }
     }
 
-    private static function getNearestRank(array $numbers, $percentile)
-    {
-        return $percentile * count($numbers) + 1 / 2;
-    }
-
+    /**
+     * @param number[] $numbers
+     * @param double $percentile
+     * @return double
+     */
     private static function nearestRank(array $numbers, $percentile)
     {
         $nearestRank = static::getNearestRank($numbers, $percentile);
@@ -38,14 +47,11 @@ final class Percentile
         return static::getValue($numbers, $nearestRank, 'end');
     }
 
-    private static function getValue(array $numbers, $key, $fallback)
-    {
-        $key = (integer) round($key) -  1;
-        $value = isset($numbers[$key]) ? $numbers[$key] : $fallback($numbers);
-
-        return (double) $value;
-    }
-
+    /**
+     * @param number[] $numbers
+     * @param double $percentile
+     * @return double
+     */
     private static function linearInterpolation(array $numbers, $percentile)
     {
         $nearestRank = static::getNearestRank($numbers, $percentile);
@@ -60,5 +66,29 @@ final class Percentile
         $right = static::getValue($numbers, $leftRank + 1, 'end');
 
         return $left + $count * ($percentile * 100 - $p1) / 100 * ($right - $left);
+    }
+
+    /**
+     * @param number[] $numbers
+     * @param double $key
+     * @param callable $fallback
+     * @return double
+     */
+    private static function getValue(array $numbers, $key, callable $fallback)
+    {
+        $key = (integer) round($key) -  1;
+        $value = isset($numbers[$key]) ? $numbers[$key] : $fallback($numbers);
+
+        return (double) $value;
+    }
+
+    /**
+     * @param number[] $numbers
+     * @param double $percentile
+     * @return double
+     */
+    private static function getNearestRank(array $numbers, $percentile)
+    {
+        return $percentile * count($numbers) + 1 / 2;
     }
 }
